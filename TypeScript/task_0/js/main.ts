b780c6a33e3aa499ee31e686bb496449d42ca92e
Francisco Antonio Baez Lopez <jessiebaez316@outlook.com>
2025-01-29 14:40:00 -0400

    main.js & ignore file upload

--- TypeScript/task_0/js/main.ts
+++ TypeScript/task_0/js/main.ts
@@ -19,7 +19,7 @@ const student2: Student = {
   location: "Juana Díaz"
 }
 
-const studentList: Array<Student> = [student1, student2]
+const studentsList: Array<Student> = [student1, student2]
 
 const table: HTMLTableElement = document.createElement('table');
 
@@ -30,12 +30,12 @@ thead.innerHTML = '<tr><th>firstName</th><th>location</th></tr>';
 table.appendChild(thead);
 const tbody: HTMLTableSectionElement = document.createElement('tbody');
 table.appendChild(tbody);
-for (let i: number = 0; i < studentList.length; i++) {
+for (let i: number = 0; i < studentsList.length; i++) {
   const row: HTMLTableRowElement = document.createElement('tr');
   const th1: HTMLTableCellElement = document.createElement('th');
   const th2: HTMLTableCellElement = document.createElement('th');
-  th1.innerHTML = studentList[i].firstName;
-  th2.innerHTML = studentList[i].location;
+  th1.innerHTML = studentsList[i].firstName;
+  th2.innerHTML = studentsList[i].location;
   row.appendChild(th1);
   row.appendChild(th2);
 
