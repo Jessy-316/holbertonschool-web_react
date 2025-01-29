d134c44426f6700f604d146b65a70abbe2c7c5c0
Francisco Antonio Baez Lopez <jessiebaez316@outlook.com>
2025-01-29 15:08:38 -0400

    main.js & ignore file upload

--- TypeScript/task_0/js/main.ts
+++ TypeScript/task_0/js/main.ts
@@ -26,21 +26,24 @@ const table: HTMLTableElement = document.createElement('table');
 document.body.appendChild(table);
 
 const thead: HTMLTableSectionElement = document.createElement('thead');
-thead.innerHTML = '<tr><th>firstName</th><th>lastName</th><th>location</th></tr>';
+thead.innerHTML = '<tr><th>firstName</th><th>lastName</th><th>age</th><th>location</th></tr>';
 table.appendChild(thead);
 const tbody: HTMLTableSectionElement = document.createElement('tbody');
 table.appendChild(tbody);
 for (let i: number = 0; i < studentsList.length; i++) {
   const row: HTMLTableRowElement = document.createElement('tr');
-  const th1: HTMLTableCellElement = document.createElement('th');
-  const th2: HTMLTableCellElement = document.createElement('th');
+  const td1: HTMLTableCellElement = document.createElement('td');
+  const td2: HTMLTableCellElement = document.createElement('td');
   const th3: HTMLTableCellElement = document.createElement('th');
-  th1.innerHTML = studentsList[i].firstName;
-  th2.innerHTML = studentsList[i].lastName;
-  th3.innerHTML = studentsList[i].location;
-  row.appendChild(th1);
-  row.appendChild(th2);
+  const td4: HTMLTableCellElement = document.createElement('td');
+  td1.innerHTML = studentsList[i].firstName;
+  td2.innerHTML = studentsList[i].lastName;
+  th3.innerHTML = String(studentsList[i].age);
+  td4.innerHTML = studentsList[i].location;
+  row.appendChild(td1);
+  row.appendChild(td2);
   row.appendChild(th3);
+  row.appendChild(td4);
 
   tbody.appendChild(row);
 }
